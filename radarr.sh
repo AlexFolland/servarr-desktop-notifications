87f3a72e9af986469cebfab7c151a069801f7ccd
Alex Folland <lexlexlex@gmail.com>
2023-06-09 10:15:13 -0700

    reduce unnecessary indentation

--- radarr.sh
+++ radarr.sh
@@ -43,8 +43,9 @@ for XUSER in "${XUSERS[@]}"; do
 	NAME=(${XUSER/(/ })
 	DISPLAY=${NAME[1]/)/}
 	DBUS_ADDRESS=unix:path=/run/user/$(id -u ${NAME[0]})/bus
-	sudo -u ${NAME[0]} DISPLAY=${DISPLAY} \
-						DBUS_SESSION_BUS_ADDRESS=${DBUS_ADDRESS} \
-						PATH=${PATH} \
-						notify-send -a "${servarr}" -u 'normal' -i "/usr/lib/${servarr,,}/bin/UI/Content/Images/logo.svg" -- "${radarr_eventtype}" "${message}"
+	sudo -u ${NAME[0]} \
+		DISPLAY=${DISPLAY} \
+		DBUS_SESSION_BUS_ADDRESS=${DBUS_ADDRESS} \
+		PATH=${PATH} \
+		notify-send -a "${servarr}" -u 'normal' -i "/usr/lib/${servarr,,}/bin/UI/Content/Images/logo.svg" -- "${radarr_eventtype}" "${message}"
 done
